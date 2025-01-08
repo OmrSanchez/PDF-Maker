e9798bd433bc1f2b980af1a4bd31352f09759f02
OmrSanchez <omar.sanchez008@gmail.com>
2025-01-08 11:09:20 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 from fpdf import FPDF
+import pandas as pd
+
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
+df = pd.read_csv("topics.csv")
 
-pdf.add_page()
+for index, row in df.iterrows():
+	pdf.add_page()
 
-pdf.set_font("Times", size=12, style="B")
-pdf.cell(w=0, h=12, txt="Hello There!", align="L", ln=1)
+	pdf.set_font("Times", size=24, style="B")
+	pdf.set_text_color(100, 100, 100)
+	pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+	pdf.line(x1=10,y1=21, x2=200, y2=21)
 
-pdf.set_font("Times", size=12)
-pdf.cell(w=0, h=12, txt="Hi There!", align="L", ln=1)
-
-pdf.add_page()
-
-pdf.set_font("Times", size=12, style="B")
-pdf.cell(w=0, h=12, txt="Hello There!", align="L", ln=1)
-
-pdf.set_font("Times", size=12)
-pdf.cell(w=0, h=12, txt="Hi There!", align="L", ln=1)
 
 pdf.output("output.pdf")
