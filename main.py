e0594fdd5d83aaf4ac777d470fa524bc44fc2c15
OmrSanchez <omar.sanchez008@gmail.com>
2025-01-08 11:19:18 -0500

    Add more pages for each topic and ensure they are labeled with the topic.

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ import pandas as pd
 
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
+
 for index, row in df.iterrows():
-	pdf.add_page()
+	for i in range(row["Pages"]):
+		pdf.add_page()
 
-	pdf.set_font("Times", size=24, style="B")
-	pdf.set_text_color(100, 100, 100)
-	pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-	pdf.line(x1=10,y1=21, x2=200, y2=21)
+		# Set the header
+		pdf.set_font("Times", size=24, style="B")
+		pdf.set_text_color(100, 100, 100)
+		pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+		pdf.line(x1=10, y1=21, x2=200, y2=21)
 
+		# Set the footer
+		pdf.ln(265)
 
+		pdf.set_font("Times", size=8, style="I")
+		pdf.set_text_color(180, 180, 180)
+		pdf.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1)
 pdf.output("output.pdf")
