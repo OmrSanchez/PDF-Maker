4e70796e62d133675ad9d1e1cf084824a90d00f4
OmrSanchez <omar.sanchez008@gmail.com>
2025-01-08 16:39:22 -0500

    Add more pages for each topic and ensure they are labeled with the topic.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ df = pd.read_csv("topics.csv")
 
 
 for index, row in df.iterrows():
-	for i in range(row["Pages"]):
+		#Generate Master pages
 		pdf.add_page()
 
 		# Set the header
@@ -18,10 +18,37 @@ for index, row in df.iterrows():
 		pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
 		pdf.line(x1=10, y1=21, x2=200, y2=21)
 
+		# Create PDF lines
+		y_one, y_two = 21, 21
+		for y in range(28):
+			y_one += 10
+			y_two += 10
+			pdf.line(x1=10, y1=y_one, x2=200, y2=y_two)
 		# Set the footer
-		pdf.ln(265)
+		pdf.ln(266)
 
 		pdf.set_font("Times", size=8, style="I")
 		pdf.set_text_color(180, 180, 180)
 		pdf.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1)
+
+		# Child page code
+		for i in range(row["Pages"] - 1):
+			pdf.add_page()
+
+			# Create PDF lines
+			y_one, y_two = 11, 11
+			pdf.line(x1=10, y1=y_one, x2=200, y2=y_two)
+			for y in range(28):
+				y_one += 10
+				y_two += 10
+				pdf.line(x1=10, y1=y_one, x2=200, y2=y_two)
+
+			# Set the footer
+			pdf.ln(278)
+
+			pdf.set_font("Times", size=8, style="I")
+			pdf.set_text_color(180, 180, 180)
+			pdf.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1)
+
+
 pdf.output("output.pdf")
